d2eb1d50a6f9e2ee60908a35b65f5e98106b30aa
Luhur Hidayat Al-Rizki <44401575+riasgry@users.noreply.github.com>
2022-09-02 08:06:32 +0700

    Update nextItem.js

--- src/component/nextItem.js
+++ src/component/nextItem.js
@@ -11,8 +11,8 @@ function NextItem() {
   return (
     <div className='page-button'>
       
-      <a className={a===1?"disablelink":""} href={'http://localhost:3000/'+b+'/'+(a-1)}><button disabled={a===1?true:false}>&lt;</button></a>
-      <a href={'http://localhost:3000/'+b+'/'+(a+1)}><button>&gt;</button></a>
+      <a className={a===1?"disablelink":""} href={'/'+b+'/'+(a-1)}><button disabled={a===1?true:false}>&lt;</button></a>
+      <a href={'/'+b+'/'+(a+1)}><button>&gt;</button></a>
     </div>
   )
 }
